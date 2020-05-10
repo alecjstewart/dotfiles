9d3817f4b5c7165a191a24ed447345efff80a6fa
Alec Stewart <alec@alecjstewart.com>
2020-05-09 20:32:45 -0600

    New aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -5,9 +5,11 @@ alias v='nvim'
 
 alias cpu='top -o cpu'
 alias grep='grep --color'
-alias pubip="curl icanhazip.com"
 alias lsd='ls -lah | grep "^d"'
 alias mem='top -o rsize' # memory
 alias please='sudo $(fc -ln -1)' # or sudo $(history -p !!) for bash
+
+alias pubip='curl icanhazip.com'
+alias localip='ifconfig | grep -Eo "inet (addr:)?([0-9]*\.){3}[0-9]*" | grep -Eo "([0-9]*\.){3}[0-9]*" | grep -v "127.0.0.1"'
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
