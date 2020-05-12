22225d3a4642ce8a8afcb2f216432b617c8493fb
Alec Stewart <alec@alecjstewart.com>
2020-05-12 14:32:36 -0600

    New git aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -11,11 +11,14 @@ alias please='sudo $(fc -ln -1)' # or sudo $(history -p !!) for bash
 
 alias gcm='git commit -m'
 alias gp='git push'
+alias gs='git status'
+alias gaa='git add .'
 
 alias pubip='curl icanhazip.com'
 alias localip='ifconfig | grep -Eo "inet (addr:)?([0-9]*\.){3}[0-9]*" | grep -Eo "([0-9]*\.){3}[0-9]*" | grep -v "127.0.0.1"'
 
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias cs='config status'
 
 brain() {
 	cd ~/Documents/brain
